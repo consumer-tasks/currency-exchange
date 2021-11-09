57047955eb37e366ce7c7ca23912da1a8512a643
ojs-hard <farlight216@gmail.com>
2021-11-09 01:11:34 -0800

    second

--- src/App.js
+++ src/App.js
@@ -1,123 +1,78 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import fetch from "unfetch";
-import useSWR from "swr";
-import {
-  Container,
-  Paper,
-  Grid,
-  TextField,
-  Select,
-  MenuItem
-} from "@material-ui/core";
-
-const API_URL = "https://api.exchangeratesapi.io";
-
-const fetcher = async path => {
-  const res = await fetch(API_URL + path);
-  const json = await res.json();
-  return json;
-};
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+import Dropdown from 'react-dropdown';
+import { HiSwitchHorizontal } from 'react-icons/hi';
+import 'react-dropdown/style.css';
+import './App.css';
 
 function App() {
-  const { data: currencies } = useSWR("/latest?base=EUR", fetcher);
 
-  const [fromValue, setFromValue] = useState(1);
-  const [toValue, setToValue] = useState(1);
-
-  const [fromCurrency, setFromCurrency] = useState("EUR");
-  const [toCurrency, setToCurrency] = useState("EUR");
-
-  const handleFromCurrencyChange = e => {
-    setFromCurrency(e.target.value);
-  };
-
-  const handleToCurrencyChange = e => {
-    setToCurrency(e.target.value);
-  };
-
-  const handleFromValueChange = e => {
-    setFromValue(parseFloat(e.target.value));
-  };
-
-  const handleToValueChange = e => {
-    setToValue(parseFloat(e.target.value));
-  };
-
-  const convertFromTo = () => {
-    const fromRate =
-      fromCurrency === "EUR" ? 1 : currencies.rates[fromCurrency];
-    const valueInEur = fromValue / fromRate;
-    const toRate = toCurrency === "EUR" ? 1 : currencies.rates[toCurrency];
-    setToValue(valueInEur * toRate);
-  };
-
-  const convertToFrom = () => {
-    const toRate = toCurrency === "EUR" ? 1 : currencies.rates[toCurrency];
-    const valueInEur = toValue / toRate;
-    const fromRate =
-      fromCurrency === "EUR" ? 1 : currencies.rates[fromCurrency];
-    setFromValue(valueInEur * fromRate);
-  };
+  const [info, setInfo] = useState([]);
+  const [input, setInput] = useState(0);
+  const [from, setFrom] = useState(['eur']);
+  const [to, setTo] = useState(['usd']);
+  const [options, setOptions] = useState([]);
+  const [output, setOutput] = useState(0);
 
   useEffect(() => {
-    convertFromTo();
-  }, [fromValue, toCurrency]);
+    Axios.get(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`)
+      .then((res) => {
+        setInfo(res.data[from]);
+      })
+  }, [from]);
 
   useEffect(() => {
-    convertToFrom();
-  }, [toValue, fromCurrency]);
+    setOptions(Object.keys(info));
+    convert();
+  }, [info])
 
-  if (!currencies) {
-    return null;
+  function convert() {
+    var rate = info[to];
+    setOutput(input * rate);
+  }
+
+  function flip() {
+    var temp = from;
+    setFrom(to);
+    setTo(temp);
   }
 
   return (
-    <Container className="currency-exchange-container" fixed>
-      <h1>Currency exchange</h1>
-      <Paper
-        className="currency-exchange-paper"
-        variant="outlined"
-        elavation={1}
-      >
-        <Grid container spacing={3}>
-          <Grid item xs={6}>
-            <TextField
-              type="number"
-              value={fromValue}
-              onChange={handleFromValueChange}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              type="number"
-              value={toValue}
-              onChange={handleToValueChange}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <Select value={fromCurrency} onChange={handleFromCurrencyChange}>
-              <MenuItem value={"EUR"}>EUR</MenuItem>
-              {Object.keys(currencies.rates).map((rate, key) => (
-                <MenuItem key={key} value={rate}>
-                  {rate}
-                </MenuItem>
-              ))}
-            </Select>
-          </Grid>
-          <Grid item xs={6}>
-            <Select value={toCurrency} onChange={handleToCurrencyChange}>
-              <MenuItem value={"EUR"}>EUR</MenuItem>
-              {Object.keys(currencies.rates).map((rate, key) => (
-                <MenuItem key={key} value={rate}>
-                  {rate}
-                </MenuItem>
-              ))}
-            </Select>
-          </Grid>
-        </Grid>
-      </Paper>
-    </Container>
+    <div className="App">
+      <div className="heading">
+        <h1>Currency Exchange Prototype</h1>
+      </div>
+      <div className='container'>
+        <div className='left'>
+          <h3>Amount</h3>
+          <input type='text'
+            placeholder='Enter the amount'
+            onChange={(e) => setInput(e.target.value)} />
+        </div>
+        <div className='middle'>
+          <h3>From</h3>
+          <Dropdown options={options}
+                    onChange={(e) => {setFrom(e.value) }}
+          value={from} placeholder='From'  />
+        </div>
+        <div className='switch'>
+          <HiSwitchHorizontal size='30px'
+          onClick={() => {flip()}} />
+        </div>
+        <div className='right'>
+          <h3>To</h3>
+          <Dropdown options={options} 
+                    onChange={(e) => {setTo(e.value)}}
+          value={to}  placeholder='To'/>
+        </div>
+      </div>
+      <div className='result'>
+        <button onClick={() => {convert()}}>Convert</button>
+        <h2>Converted Amount: </h2>
+        <p>{input+' '+ from+' = '+output.toFixed(2) + ' ' + to}</p>
+      </div>
+    </div>
   );
 }
 
